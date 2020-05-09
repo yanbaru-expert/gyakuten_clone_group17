ae751e667e7574322de00689117389b612c0cc46
shinichi <0503baku@gmail.com>
2020-05-10 00:17:37 +0900

    質問集ページの実装_2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,36 @@
 class QuestionsController < ApplicationController
 
+  # 新規作成画面に対応するアクション
+  def new
+    @question = Question.new
+  end
+
+  # 一覧画面に対応するアクション
   def index
-    @question = Question.all
+    @questions = Question.all
   end
   
+  # 新規登録するためのアクション
+  def create
+    Question.create(question_params)
+  end
+
+  # 削除を行うためのアクション
+  def destroy
+    question = Question.find(params[:id])
+    question.delete
+  end
+
+private
+
+    def set_question
+      @question = Question.find(params[:id])
+    end
+
+    def question_params
+      params.require(:question).permit(:title, :detail)
+    end
+
 end
+
+   

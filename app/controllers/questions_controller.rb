ca06aba3746ca34e613cb7795ec237d88efd94ee
soma-maruuchi <mwa60346@gmail.com>
2020-05-11 22:32:17 +0900

    登録成功・失敗機能実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,11 +4,15 @@ class QuestionsController < ApplicationController
     @questions = Question.all.order(id: "DESC")
   end
 
-  def new
-  end
-
   def create
     @question = Question.new(question_params)
+    if @question.save
+      redirect_to action :"index"
+    else
+      flash.now[:alert] = "登録に失敗しました"
+      @questions = Question.all.order(created_at: :"DESC")
+      render :index
+    end
   end
 
   private

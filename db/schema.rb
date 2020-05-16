6aced951b344a4b56a6ab514eb61172d9c35b004
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-05-16 23:35:00 +0900

    titleカラム削除

--- db/schema.rb
+++ db/schema.rb
@@ -65,7 +65,6 @@ ActiveRecord::Schema.define(version: 2020_05_15_124556) do
   end
 
   create_table "solutions", force: :cascade do |t|
-    t.string "title"
     t.text "detail"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false

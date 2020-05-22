d3ecdd290bfe2c3bbf04367474a808e107b92f8b
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-05-22 14:15:49 +0900

    35_テキスト教材のrakeタスクを実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_113842) do
+ActiveRecord::Schema.define(version: 2020_05_22_025610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,15 +66,20 @@ ActiveRecord::Schema.define(version: 2020_05_20_113842) do
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "detail"
+    t.string "title"
+    t.text "detail"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer  "reviews_count", default: 0
+    t.integer "reviews_count", default: 0
   end
 
   create_table "solutions", force: :cascade do |t|
     t.text "detail"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "texts", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

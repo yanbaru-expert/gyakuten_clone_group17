0ca2aa9f54278a34310387cb2a7ea49ff066aa75
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-05-26 20:47:23 +0900

    test

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -2,9 +2,4 @@ class TextsController < ApplicationController
     def index
         @texts = Text.all
     end
-
-    def search
-        @texts = Text.where('title LIKE(?)', "%#{params[:keyword]}%")
-        render json: @texts
-    end
 end
--- config/webpack/text.js
+++ config/webpack/text.js
@@ -1,16 +1,5 @@
-document.addEventListener('turbolinks:load',function() {
-    $("#search-text").keyup(function(){
-        var val = $(this).val();
-        $('.card-title').each(function() {
-            if($(this).text().toLowerCase().indexOf(val.toLowerCase()) != -1){
-                $(this).parents('#text').removeClass("hidden")
-                $(this).parents('#text').addClass("d-flex")
-            }else{
-                $(this).parents('#text').removeClass("d-flex")
-                $(this).parents('#text').addClass("hidden")
-            }
-        });
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
-    });
+const environment = require('./environment')
 
-});
+module.exports = environment.toWebpackConfig()

615d3975ca280a2c07f599fb23ede178e97ef99c
shinichi <0503baku@gmail.com>
2020-05-27 21:38:01 +0900

    add_2

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,11 +1,12 @@
 class SolutionsController < ApplicationController
  def create
-    @question = Question.new(question_params)
-    if @question.save
+    @question = Question.new(question_params[:id])
+    if @solution.save
       redirect_to questions_path, notice: "質問を投稿しました"
     else
       flash.now[:alert] = "エラーが発生しました"
       @questions = Question.all
       render :index
     end
-  end
+ end
+end

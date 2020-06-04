fe07012672fb8f81483d35d1c3c692d8fec5343a
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-06-04 09:17:49 +0900

    38_管理者画面を利用したコンテンツの追加機能の実装

---
+++ app/admin/questions.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register Question do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :title, :detail, :reviews_count
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :detail, :reviews_count]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+end

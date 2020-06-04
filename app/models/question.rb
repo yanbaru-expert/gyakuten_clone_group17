c1ab40fc30be976d9114885e2880b9ad2a681f6d
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-06-04 21:33:33 +0900

    修正

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,4 @@ class Question < ApplicationRecord
   validates :title, presence: { message: "は空欄にできません" }
   validates :detail, presence: { message: "は空欄にできません" }
   has_many :solutions
-  has_many :reviews
 end
